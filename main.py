7cdd50aace5b13ea052477151abb35eaa7dbc773
brian.musili <musilibrian07@gmail.com>
2026-01-20 07:03:29 +0300

    feat: basic workflow to generate x post

--- main.py
+++ main.py
@@ -1,36 +1,33 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # type: ignore
 import requests
 import os
-
 
 load_dotenv()
 
 
-# API Configuration
 API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
 API_KEY = os.getenv("GEMINI_API_KEY")
 
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+    You are an expert social media manager, and excel in creating engaging posts for X (formerly Twitter).
+    
+    Your task is to generate a post that is engaging, concise, and relevant to the topic provided by the user.
+    
+    Avoid using hashtags and lots of emojis. Focus on clarity and engagement.
+    
+    Keep the post short and focused, structure it using line breaks and empty lines to enhance readability.
+    
+    Here is the topic for the post: 
+    
+    <TOPIC>
+    {topic}
+    </TOPIC>
+    """
 
-def generate_x_post(user_input: str) -> str:
-    #     curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent" \
-    #   -H "x-goog-api-key: $GEMINI_API_KEY" \
-    #   -H 'Content-Type: application/json' \
-    #   -X POST \
-    #   -d '{
-    #     "contents": [
-    #       {
-    #         "parts": [
-    #           {
-    #             "text": "Explain how AI works in a few words"
-    #           }
-    #         ]
-    #       }
-    #     ]
-    #   }'
+    payload = {"contents": [{"parts": [{"text": prompt}]}]}
 
-    payload = {"contents": [{"parts": [{"text": user_input}]}]}
-
-    requests.post(
+    response = requests.post(
         API_URL,
         json=payload,
         headers={
@@ -39,19 +36,24 @@ def generate_x_post(user_input: str) -> str:
         },
     )
 
-    pass
-
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            return data['candidates'][0]['content']['parts'][0]['text']
+        except (KeyError, IndexError):
+            return "Error parsing API response."
+    else:
+        return f"Error: API request failed with status code {response.status_code}"
 
 def main():
-    # print("Hello from essentials!")
-
-    user_input = input("What should the post be about?")
-
+    user_input = input("What should the post be about? ")
+    
     x_post = generate_x_post(user_input)
 
-    print("Generated Post:")
+    print("\nGenerated Post:")
+    print("------------------------------------------------")
     print(x_post)
-
+    print("------------------------------------------------")
 
 if __name__ == "__main__":
     main()
